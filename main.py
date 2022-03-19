3121a4fd044bd23e2da68fcae02d0aee1faab73f
Valerii3 <vallerus55@gmail.com>
2022-03-19 17:36:58 +0300

    print trades

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+
+def get_trades(coin1="btc", coin2="usd", limit=150):
+    response = requests.get(url=f"https://yobit.net/api/3/trades/{coin1}_{coin2}?limit={limit}&ignore_invalid=1")
+
+    with open("trades.txt", "w") as file:
+        file.write(response.text)
+
+    total_trade_ask = 0
+    total_trade_bid = 0
+
+    for item in response.json()[f"{coin1}_{coin2}"]:
+        if item["type"] == "ask":
+            total_trade_ask += item["price"] * item["amount"]
+        else:
+            total_trade_bid += item["price"] * item["amount"]
+
+    info = f"[-] TOTAL {coin1} SELL: {round(total_trade_ask, 2)} $\n[+] TOTAL {coin1} BUY: {round(total_trade_bid, 2)} $"
+
+    return info
+
+
+def main():
+    coin = input()
+    print(get_trades(coin))
+
+
+
+if __name__ == '__main__':
+    main()
